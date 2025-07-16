ce44eb0d359daa0db345ad175be6ee5206553741
shw <dustmzmfla@naver.com>
2025-07-17 02:43:10 +0900

    세팅

--- src/components/layouts/FixedHeader.tsx
+++ src/components/layouts/FixedHeader.tsx
@@ -1,16 +1,18 @@
+import "../../style/layouts/fixedHeader.css"
 
 interface BannerSettings {
     bannerBg : string;
     bannerText : string;
     textColor: string;
     textBold : string;
-    
+    fontSize: number;
+    link: string;
 }
 
 function FixedHeader() {
     return (
         // 맨 상단 배너
-        <div className="fixedHeader">asdasd</div>
+        <a href="#" id="fixedHeader">이벤트 배너</a>
     )
 }
 
