5a6ed87ee8d1ac7f75755aed193b315e4b5d8297
shw <dustmzmfla@naver.com>
2025-07-17 01:58:36 +0900

    폰트 프리텐다드 적용

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import FixedHeader from './components/layouts/FixedHeader';
+
 function App() {
   return (
     <div className="App">
-      
+      <FixedHeader/>
     </div>
   );
 }
--- src/components/layouts/FixedHeader.tsx
+++ src/components/layouts/FixedHeader.tsx
@@ -1,7 +1,16 @@
 
+interface BannerSettings {
+    bannerBg : string;
+    bannerText : string;
+    textColor: string;
+    textBold : string;
+    
+}
+
 function FixedHeader() {
     return (
-        <></>
+        // 맨 상단 배너
+        <div className="fixedHeader">asdasd</div>
     )
 }
 
