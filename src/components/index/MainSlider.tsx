9b1e90ec52de7ce4ce5f9a2c6f427c6043a16548
shw <dustmzmfla@naver.com>
2025-07-21 05:37:34 +0900

    중단 버튼 추가 및 클릭 이벤트 추가

--- src/components/index/MainSlider.tsx
+++ src/components/index/MainSlider.tsx
@@ -1,12 +1,33 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
+import { Swiper, SwiperClass, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Scrollbar, Autoplay } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 import IconArrowRight from '../../icons/IconArrowRight';
+import IconArrowLeft from '../../icons/IconArrowLeft';
+import { useRef, useState } from 'react';
+import { Swiper as SwiperCore } from 'swiper';
 
 function MainSlider () {
+    // 일시정지 버튼 "my-button-pause"
+    const [paused, setPaused] = useState(false);
+
+    //
+    const swiperRef = useRef<SwiperCore | null>(null);
+
+    const togglePause = () => {
+        if (!swiperRef.current) return;
+
+        if (paused) {
+            swiperRef.current.autoplay.start();
+        }else {
+            swiperRef.current.autoplay.stop();
+        }
+
+        setPaused(prev => !prev);
+    };
+
     return (
         <>
             <Swiper
@@ -26,6 +47,9 @@ function MainSlider () {
                     delay: 5000,
                     disableOnInteraction: false,
                 }}
+                onSwiper={(swiper: SwiperClass) => {
+                    swiperRef.current = swiper;
+                }}
                 className='mainSlider'
             >
                 <SwiperSlide>Slide 1</SwiperSlide>
@@ -35,7 +59,12 @@ function MainSlider () {
             </Swiper>
         
             <div className="my-navigation-buttons">
-                <button className="my-button-prev"></button>
+                <button className="my-button-prev">
+                    <IconArrowLeft  color="var(--color-black)" height={30} width={30}/>
+                </button>
+                <button className={`my-button-pause ${paused ? 'paused' : ''}`} onClick={togglePause}>
+
+                </button>
                 <button className="my-button-next">
                     <IconArrowRight color="var(--color-black)" height={30} width={30}/>
                 </button>
