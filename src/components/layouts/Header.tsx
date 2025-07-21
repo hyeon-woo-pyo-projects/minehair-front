00624585c51e334e2cc753201706182649ce4885
shw <dustmzmfla@naver.com>
2025-07-21 19:48:04 +0900

    브랜드 컬러 수정

--- src/components/layouts/Header.tsx
+++ src/components/layouts/Header.tsx
@@ -6,7 +6,7 @@ function Header () {
         <BrowserRouter>
             <header>
                 <div id='headerLine'>
-                    <Link to="/" id='headerLogo'>
+                    <Link to="/" id="headerLogo">
                         <img src="" alt="민이헤어_로고" />
                     </Link>
                 </div>
